5cf0a1469fbb4c4720384124b2940b4fe0aa0699
Preyansh Jain <preyansh.jain2@cognizant.com>
2024-09-24 13:14:13 +0530

    first commit

---
+++ screens/LoginScreen.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { Alert } from "react-native";
+import AuthContent from "../components/Auth/AuthContent";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { login } from "../util/auth";
+
+function LoginScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication Failed!",
+        "Could not log you in. Please check credentials"
+      );
+    }
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Logging you in..." />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
+}
+
+export default LoginScreen;
